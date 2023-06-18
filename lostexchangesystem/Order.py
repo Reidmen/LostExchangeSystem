5b08aef0af8b742ef5463a5b45e0be57f7ba3148
Reidmen <r.rethmawn@gmail.com>
2023-06-18 21:47:48 +0200

    updated order class

--- lostexchangesystem/Order.py
+++ lostexchangesystem/Order.py
@@ -8,89 +8,90 @@ from datetime import datetime
 from Constants import OrderStatus, OrderType, TimeEnforcementType
 
 
-class Order(ABC):
-    def __init__(self, _id: str):
+class OrderBase(ABC):
+    @property
+    @abstractmethod
+    def order_id(self):
+        pass
+
+    @property
+    @abstractmethod
+    def is_bid(self):
+        pass
+
+    @property
+    @abstractmethod
+    def previous_item(self):
+        pass
+
+    @property
+    @abstractmethod
+    def next_item(self):
+        pass
+
+    @property
+    @abstractmethod
+    def root(self):
+        pass
+
+
+class Order(OrderBase):
+    """Double-Linked list order item."""
+
+    def __init__(
+        self,
+        order_id: str = "",
+        is_bid: bool = False,
+        size: float = 0.0,
+        price: float = 0.0,
+        root: OrderBase | None = None,
+        next_item: OrderBase | None = None,
+        previous_item: OrderBase | None = None,
+    ) -> None:
         """Initializes order as open, and defines basic attributes
         such as id, is_buy, status and time_enforcement."""
-        self.__order_id = _id
-        self.__is_buy_order = False
+        self.__order_id = order_id
+        self.__is_bid = False
+        self.__size = size
+        self.__price = price
         self.__status = OrderStatus.OPEN
         self.__time_enforcement = TimeEnforcementType.ON_THE_OPEN
         self.__started_time = datetime.now()
         self.__closed_time = None
 
+        self.root = root
+        self.next_item = next_item
+        self.previous_item = previous_item
+
     @property
-    @abstractmethod
     def order_id(self):
         return self.__order_id
 
     @property
-    @abstractmethod
-    def is_buy_order(self):
-        return self.__is_buy_order
+    def is_bid(self):
+        return self.__is_bid
 
     @property
-    @abstractmethod
-    def status(self):
-        return self.__status
+    def size(self):
+        return self.__size
 
-    @abstractmethod
-    def set_new_status(self, status: OrderStatus):
-        """Encapsulates editing the order status, which includes
-        cancelling"""
-        self.__status = status
+    def append(self, order: OrderBase) -> None:
+        """Append an order object"""
+        if self._next_item is None:
+            self.next_item = order
+            self.next_item.previous_item = self
+            self.next_item.root = self.root
 
-    @abstractmethod
-    def set_close_time(self, time: datetime):
-        """Set closed time for order in case cancelled
-        or fully/partially filled."""
-        self.__closed_time = time
+            self.root.count += 1
+            self.root.tail = order
 
 
-class SellOrderWithLimit(Order):
+class SellOrderWithLimit(OrderBase):
     def __init__(self, _id, quantity, limit_price, enforcement_type):
         self.__order_id = _id
         self.__is_buy_order = False
         self.__status = OrderStatus.OPEN
         self.__order_type = OrderType.SELL
-        self.__time_enforcement = TimeEnforcementType.ON_THE_OPEN
-        self.__started_time = datetime.now()
-        self.__closed_time = None
-        self.__limit_price = limit_price
-
-    @property
-    def order_id(self):
-        return self.__order_id
-
-    @property
-    def is_buy_order(self):
-        return self.__is_buy_order
-
-    @property
-    def status(self):
-        return self.__status
-
-    def save_to_database(self):
-        """Save order to database."""
-        return NotImplementedError("Instantiate databased (NOSQL / H5)")
-
-    def set_new_status(self, status: OrderStatus):
-        """Encapsulates editing the order status, which includes
-        cancelling"""
-        self.__status = status
-
-    def set_close_time(self, time: datetime):
-        """Set closed time for order in case cancelled
-        or fully/partially filled."""
-        self.__closed_time = time
-
-
-class BuyOrderWithLimit(Order):
-    def __init__(self, _id, quantity, limit_price, enforcement_type):
-        self.__order_id = _id
-        self.__is_buy_order = False
-        self.__status = OrderStatus.OPEN
-        self.__order_type = OrderType.BUY
         self.__time_enforcement = TimeEnforcementType.ON_THE_OPEN
         self.__started_time = datetime.now()
         self.__closed_time = None
