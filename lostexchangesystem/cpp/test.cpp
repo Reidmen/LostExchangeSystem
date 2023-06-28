3e2f598a7319503ccdffa18b61e7d485f896e27c
Reidmen <r.rethmawn@gmail.com>
2023-06-28 19:25:44 +0200

    added extra testing (wip)

--- lostexchangesystem/cpp/test.cpp
+++ lostexchangesystem/cpp/test.cpp
@@ -34,3 +34,25 @@ std::shared_ptr<Limit> createDummyTree(std::shared_ptr<Limit> dummyA,
     statusCode = addLimit(ptr_root, dummyD);
     assert(statusCode == 1);
 };
+
+// asserting on equality of Limit structures
+void AssertPtrEquals(std::shared_ptr<Limit> ptr_firstLimit,
+                     std::shared_ptr<Limit> ptr_secondLimit){};
+
+// test dummy tree
+void TestCreateDummyTree() {
+    std::shared_ptr<Limit> ptr_limitA = createDummyLimit(100.0);
+    std::shared_ptr<Limit> ptr_limitB = createDummyLimit(200.0);
+    std::shared_ptr<Limit> ptr_limitC = createDummyLimit(20.0);
+    std::shared_ptr<Limit> ptr_limitD = createDummyLimit(50.0);
+
+    std::shared_ptr<Limit> ptr_root =
+        createDummyTree(ptr_limitA, ptr_limitB, ptr_limitC, ptr_limitD);
+
+    // TODO assert equality of pointers information between tree and limits
+    AssertPtrEquals(ptr_limitA, ptr_root->rightChild);
+    AssertPtrEquals(ptr_limitC, ptr_limitA->leftChild);
+};
+
+// test on pushing
+void TestOrderPushing(){};
