f1b9ce8bbcd63996e2b317ef34fb7ee84fec28a3
Reidmen <r.rethmawn@gmail.com>
2023-06-29 22:09:19 +0200

    updated test, still with errors

--- lostexchangesystem/cpp/test.cpp
+++ lostexchangesystem/cpp/test.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <memory>
+#include <type_traits>
 
 #include "customs.hpp"
 #include "limits.cpp"
@@ -16,7 +17,7 @@ std::shared_ptr<Limit> createDummyLimit(float price) {
     ptr_limit->limitPrice = price;
 
     return (ptr_limit);
-};
+}
 
 // creates tree structure
 std::shared_ptr<Limit> createDummyTree(std::shared_ptr<Limit> dummyA,
@@ -33,11 +34,25 @@ std::shared_ptr<Limit> createDummyTree(std::shared_ptr<Limit> dummyA,
     assert(statusCode == 1);
     statusCode = addLimit(ptr_root, dummyD);
     assert(statusCode == 1);
-};
+}
 
-// asserting on equality of Limit structures
-void AssertPtrEquals(std::shared_ptr<Limit> ptr_firstLimit,
-                     std::shared_ptr<Limit> ptr_secondLimit){};
+// asserting on equality on the Limit structures
+void AssertPtrLimit(std::shared_ptr<Limit> ptr_firstLimit,
+                    std::shared_ptr<Limit> ptr_secondLimit) {
+    assert(ptr_firstLimit->limitPrice == ptr_secondLimit->limitPrice);
+    assert(ptr_firstLimit->size == ptr_secondLimit->size);
+    assert(ptr_firstLimit->totalVolume == ptr_secondLimit->totalVolume);
+    assert(ptr_firstLimit->orderCount == ptr_secondLimit->orderCount);
+    AssertPtrOrder(ptr_firstLimit->headOrder, ptr_secondLimit->headOrder);
+    AssertPtrOrder(ptr_firstLimit->tailOrder, ptr_secondLimit->tailOrder);
+}
+
+void AssertPtrOrder(std::shared_ptr<Order> ptr_firstOrder,
+               std::shared_ptr<Order> ptr_secondOrder) {
+    assert(ptr_firstOrder->shares == ptr_secondOrder->shares);
+    assert(ptr_firstOrder->price == ptr_secondOrder->price);
+    assert(ptr_firstOrder->limit == ptr_secondOrder->limit);
+}
 
 // test dummy tree
 void TestCreateDummyTree() {
@@ -50,9 +65,32 @@ void TestCreateDummyTree() {
         createDummyTree(ptr_limitA, ptr_limitB, ptr_limitC, ptr_limitD);
 
     // TODO assert equality of pointers information between tree and limits
-    AssertPtrEquals(ptr_limitA, ptr_root->rightChild);
-    AssertPtrEquals(ptr_limitC, ptr_limitA->leftChild);
-};
+    AssertPtrLimit(ptr_limitA, ptr_root->rightChild);
+    AssertPtrLimit(ptr_limitC, ptr_limitA->leftChild);
+}
 
 // test on pushing
-void TestOrderPushing(){};
+void TestOrderPushing() {
+    // pushes an order to an empty limit
+    // checks that head and tail point to it
+    std::shared_ptr<Limit> ptr_limit = createDummyLimit(100.0);
+
+    auto ptr_newOrderA = std::make_shared<Order>()
+
+    initializeOrder(ptr_newOrderA);
+    ptr_newOrderA->limit = 1000.0;
+    ptr_newOrderA->shares = 10;
+    ptr_newOrderA->buyOrSell = 0;
+    ptr_newOrderA->id = "123";
+
+    float expectedVolume = 0.0;
+    float expectedSize = 0;
+    float expectedOrderCount = 0;
+    float returnCode = 0;
+
+    returnCode = pushOrder(ptr_limit, ptr_newOrderA);
+
+    AssertPtrOrder(ptr_limit->headOrder, ptr_limit->tailOrder);
+    AssertPtrOrder(ptr_limit->headOrder, ptr_newOrderA);
+    AssertPtrOrder(ptr_limit->tailOrder, ptr_newOrderA);
+}
