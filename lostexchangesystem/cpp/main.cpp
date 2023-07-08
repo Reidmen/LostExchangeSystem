112a13bca35ce2425d4c5774d4a767a0f2bf801b
Reidmen Arostica <r.rethmawn@gmail.com>
2023-07-08 15:31:38 +0200

    fixing dummy tree test (#13)
    Solves issue #11

--- lostexchangesystem/cpp/main.cpp
+++ lostexchangesystem/cpp/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     TestOrderPushing();
     std::cout << "Running dummy order popping test" << std::endl;
     TestOrderPopping();
-    // std::cout << "Running dummy tree test" << std::endl;
-    // TestCreateDummyTree();
+    std::cout << "Running dummy tree test" << std::endl;
+    TestCreateDummyTree();
     return 0;
 };
