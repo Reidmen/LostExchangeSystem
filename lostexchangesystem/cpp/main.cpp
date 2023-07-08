eadbb2d7cbcba256b4aebf8bf466d34c374719f1
Reidmen <r.rethmawn@gmail.com>
2023-07-08 12:10:13 +0200

    Fixing (#9) test for add and pop orders

--- lostexchangesystem/cpp/main.cpp
+++ lostexchangesystem/cpp/main.cpp
@@ -1,9 +1,13 @@
 #include "customs.hpp"
 #include "test.cpp"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-    TestCreateDummyTree();
+    std::cout << "Running dummy order pushing test" << std::endl;
     TestOrderPushing();
-
+    std::cout << "Running dummy order popping test" << std::endl;
+    TestOrderPopping();
+    // std::cout << "Running dummy tree test" << std::endl;
+    // TestCreateDummyTree();
     return 0;
 };
